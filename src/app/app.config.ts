9a9bab2f9c03ef002cf2ed8e9eecd3e0974fcf3b
MahmoudSayed889 <msayedm889@gmail.com>
2026-09-23 16:01:39 +0300

    init

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,32 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withInMemoryScrolling, withViewTransitions } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration()
-  ]
+    providers: [
+        provideBrowserGlobalErrorListeners(),
+        provideRouter(
+            routes, 
+            withViewTransitions(),
+            withInMemoryScrolling({
+                scrollPositionRestoration: 'enabled',
+                anchorScrolling: 'enabled'
+            })
+        ), 
+        provideClientHydration(),
+        provideHttpClient(),
+        providePrimeNG({
+            theme: {
+                preset: Aura,
+                options: {
+                    darkModeSelector: 'dark',
+                }
+            }
+        })
+    ]
 };
